a9cf708c36829f5a83b354184f89df00604ecf73
Guilherme Morais <gui.lopes@gmail.com>
2019-08-29 21:24:11 +0200

    Stetic changes.

--- client/src/App.js
+++ client/src/App.js
@@ -11,8 +11,8 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className='App-title'> Fib Calculator</h1>
-          <Link to='/'>Home</Link><br />
+          <h1 className='App-title'> Fib Calculator </h1>
+          <Link to='/'>Home</Link> | 
           <Link to='/otherPage'>Other Page</Link>
         </header>
         <div>
--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default () => {
     return(
         <div>
-            I'm some other page! 1.2 <br/>
+            I'm some other page! 1.3 <br/>
             <Link to="/">Go back home</Link>
         </div>
     )
